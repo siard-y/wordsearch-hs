a7d8015f41b1cc8177a767f2c64b758c6fa9eb39
Siard <siardypma@gmail.com>
2024-01-16 23:58:38 +0100

    Add CLI argument parser with optparse-applicative

--- app/Main.hs
+++ app/Main.hs
@@ -3,7 +3,31 @@ module Main where
 import Options.Applicative
 import Lib
 
+data FileInput = FileInput
+  { gridFile    :: FilePath
+  , wordsFile   :: FilePath }
+
+fileInput :: Parser FileInput
+fileInput = FileInput
+      <$> strOption
+          (  long "grid"
+          <> short 'g'
+          <> metavar "GRID_FILE"
+          <> help "Word search grid file" )
+      <*> strOption
+          (  long "words"
+          <> short 'w'
+          <> metavar "WORDS_FILE"
+          <> help "Word search word list file" )
+
+run :: FileInput -> IO ()
+run options = putStrLn "placeholder"
+
 main :: IO ()
-main = do
-  putStrLn "Hallo liebe Leute!"
-  Lib.someFunc
+main = run =<< execParser opts
+  where
+    opts = info (fileInput <**> helper)
+      ( fullDesc
+     <> progDesc "Find words in a word search puzzle"
+     <> header "Word search puzzle solver" )
+     
